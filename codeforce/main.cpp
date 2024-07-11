d625ea6795506b00e285e5eca97df65c846806a9
jackson <aaa9471@gmail.com>
2024-07-11 21:57:11 +0800

    Initial Commit

---
+++ codeforce/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  codeforce
+//
+//  Created by Yeh, Hsuan-Wei on 2024/7/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
